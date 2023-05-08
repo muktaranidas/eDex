4939b464bed8dfd55b8f6e81c39d6d1ed9072c16
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-05-08 03:18:48 -0700

    complete banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,51 +1,30 @@
 import React from "react";
+import img from "../../../assets/banner/banner.png";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="hero">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+    <section className="bg-[#6176F7] ">
+      <div className="container widthClass flex flex-col  lg:flex-row">
+        <div className="flex flex-col justify-center mt-[178px]">
+          <p className="banner-h4">START TO SUCCESS</p>
+          <h1 className="banner-heading my-[24px]">
+            Access To 5000+ Course from 300 Instructors & Institutions
+          </h1>
+          <p className="banner-p mb-[32px]">
+            Various versions have evolved over the years, sometimesbyaccident,
           </p>
+          <input
+            type="text"
+            placeholder="What do want to learn?"
+            className="input mb-[318px] py-[24px] pl-[32px] pr-[373px] banner-input"
+          />
         </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </div>
+        <div className="flex -mt-16 mb-[248px] ">
+          <img src={img} alt="" />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,7 @@ import About from "./About/About";
 const Home = () => {
   return (
     <div>
-      {/* <Banner></Banner> */}
+      <Banner></Banner>
       <Course></Course>
       <PopularCategory></PopularCategory>
       <Instructor></Instructor>
